d24ee9deddf7e10c3fa30f44272730e1b8610e6e
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-08-12 08:57:58 -0700

    Version 0.6.3.

--- email-download.php
+++ email-download.php
@@ -3,7 +3,7 @@
  * Plugin Name: Email Download
  * Plugin URI: https://github.com/dwnload/wp-email-download
  * Description: Allow users to download any WordPress managed file if they're subscribed to you MailChimp list.
- * Version: 0.6.2
+ * Version: 0.6.3
  * Requires PHP: 7.4
  * Author:  Austin Passy
  * Author URI: https://austin.passy.co
