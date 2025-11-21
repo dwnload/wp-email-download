6b40fb74146bb415abd268430945cf3cb8011fce
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-21 08:01:32 -0800

    Unnecessary curly braces

--- src/Api/Api.php
+++ src/Api/Api.php
@@ -152,7 +152,7 @@ final class Api
 
         return $wpdb->query(
             $wpdb->prepare(
-                "DELETE FROM {$wpdb->options} WHERE option_name LIKE %s OR option_name LIKE %s",
+                "DELETE FROM $wpdb->options WHERE option_name LIKE %s OR option_name LIKE %s",
                 $wpdb->esc_like('_transient_dwnload/mailchimp_lists_') . '%',
                 $wpdb->esc_like('_transient_timeout_dwnload/mailchimp_lists_') . '%'
             )
