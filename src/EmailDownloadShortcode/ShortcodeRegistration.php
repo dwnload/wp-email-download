9900c8a0aa8a5d6739d5bbee500c8fb52606927d
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-21 16:24:06 -0800

    Move into contructor prop and code cleanup

--- src/EmailDownloadShortcode/ShortcodeRegistration.php
+++ src/EmailDownloadShortcode/ShortcodeRegistration.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Dwnload\WpEmailDownload\EmailDownloadShortcode;
 
 use Dwnload\WpEmailDownload\ShortcodeApi\AbstractShortcode;
@@ -8,30 +10,26 @@ use TheFrosty\WpUtilities\Plugin\WpHooksInterface;
 
 /**
  * Class ShortcodeRegistration
- *
  * @package Dwnload\WpEmailDownload\ShortcodeApi\EmailDownloadShortcode
  */
-class ShortcodeRegistration extends AbstractShortcode implements WpHooksInterface {
-
-    /**
-     * @var ShortcodeInterface $shortcode
-     */
-    protected $shortcode;
+class ShortcodeRegistration extends AbstractShortcode implements WpHooksInterface
+{
 
     /**
      * ShortcodeRegistration constructor.
-     *
      * @param ShortcodeInterface $shortcode
      */
-    public function __construct( ShortcodeInterface $shortcode ) {
-        $this->shortcode = $shortcode;
+    public function __construct(protected ShortcodeInterface $shortcode)
+    {
     }
 
-    public function addHooks(): void {
-        add_action( 'init', [ $this, 'addShortcode' ] );
+    public function addHooks(): void
+    {
+        add_action('init', [$this, 'addShortcode']);
     }
 
-    public function addShortcode() {
-        add_shortcode( $this->shortcode->getTag(), [ $this->shortcode->getHandler(), 'handler' ] );
+    public function addShortcode(): void
+    {
+        add_shortcode($this->shortcode->getTag(), [$this->shortcode->getHandler(), 'handler']);
     }
 }
