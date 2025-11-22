663d1c7a806281927482a1bfc1c407d4eb689642
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-21 16:27:02 -0800

    PHP 83 code cleanup

--- src/Api/SubscriptionController.php
+++ src/Api/SubscriptionController.php
@@ -1,58 +1,53 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Dwnload\WpEmailDownload\Api;
 
 use Dwnload\WpEmailDownload\EmailDownload;
 use Dwnload\WpEmailDownload\Http\Services\RegisterPostRoute;
 use Dwnload\WpSettingsApi\Api\Options;
+use Exception;
+use WP_Error;
+use WP_Http;
 use WP_REST_Request;
 use WP_REST_Response;
 
 /**
  * Class SubscriptionController
- *
  * @package Dwnload\WpEmailDownload\Api
  */
-class SubscriptionController extends RegisterPostRoute {
+class SubscriptionController extends RegisterPostRoute
+{
 
-    const ROUTE_PREFIX = '/user/';
-    const ROUTE_REQUIRED_FIELD = 'email';
-    const DOWNLOAD_KEY = 'file_id';
-
-    /** @var Api $api */
-    protected $api;
+    const string ROUTE_PREFIX = '/user/';
+    const string ROUTE_REQUIRED_FIELD = 'email';
+    const string DOWNLOAD_KEY = 'file_id';
 
     /**
      * SubscriptionController constructor.
-     *
      * @param Api $api
      */
-    public function __construct( Api $api ) {
-        $this->api = $api;
-    }
-
-    /**
-     * Add class hooks.
-     */
-    public function addHooks(): void {
-        parent::addHooks();
+    public function __construct(protected Api $api)
+    {
     }
 
     /**
      * Registers a REST API route.
      * @todo add permission_callback to $args param of registerRoute.
      */
-    public function initializeRoute() {
+    public function initializeRoute(): void
+    {
         $this->registerRoute(
             EmailDownload::ROUTE_NAMESPACE,
             self::ROUTE_PREFIX . "(?P<" . self::ROUTE_REQUIRED_FIELD . ">\S+)",
-            [ $this, 'validateUserEmailSubscription' ],
+            [$this, 'validateUserEmailSubscription'],
             [
                 'args' => [
                     self::ROUTE_REQUIRED_FIELD => [
                         'required' => true,
-                        'validate_callback' => function( $value ): bool {
-                            return $this->api->isValidEmail( $value );
+                        'validate_callback' => function ($value): bool {
+                            return $this->api->isValidEmail($value);
                         },
                     ],
                 ],
@@ -62,63 +57,72 @@ class SubscriptionController extends RegisterPostRoute {
 
     /**
      * @param WP_REST_Request $request
-     *
-     * @return WP_REST_Response|mixed
+     * @return WP_REST_Response
      */
-    public function validateUserEmailSubscription( WP_REST_Request $request ) {
-        if ( ! check_ajax_referer( self::NONCE_ACTION, false, false ) ) {
-            return rest_ensure_response( new \WP_Error(
-                'nonce_error',
-                'A valid nonce key is required, please try again.',
-                [ 'status' => \WP_Http::UNAUTHORIZED ]
-            ) );
+    public function validateUserEmailSubscription(WP_REST_Request $request): WP_REST_Response
+    {
+        if (!check_ajax_referer(self::NONCE_ACTION, false, false)) {
+            return rest_ensure_response(
+                new WP_Error(
+                    'nonce_error',
+                    'A valid nonce key is required, please try again.',
+                    ['status' => WP_Http::UNAUTHORIZED]
+                )
+            );
         }
 
         $data = [
             'success' => false,
-            'date' => date( 'Y-m-d H:i:s' ),
+            'date' => date('Y-m-d H:i:s'),
         ];
 
         // Required parameters (though the 'email' field is required by the route
-        if ( empty( $request->get_param( self::ROUTE_REQUIRED_FIELD ) ) ||
-             empty( $request->get_param( Mailchimp::LIST_ID ) )
+        if (
+            empty($request->get_param(self::ROUTE_REQUIRED_FIELD)) ||
+            empty($request->get_param(Mailchimp::LIST_ID))
         ) {
-            return rest_ensure_response( new \WP_Error(
-                'missing_params',
-                'One or more parameters are missing from this request.',
-                [ 'status' => \WP_Http::OK ]
-            ) );
+            return rest_ensure_response(
+                new WP_Error(
+                    'missing_params',
+                    'One or more parameters are missing from this request.',
+                    ['status' => WP_Http::OK]
+                )
+            );
         }
 
         // This is here for extra protection (not for users) Admins show have their keys set
-        if ( empty( $api_key = Options::getOption( Mailchimp::SETTING_API_KEY ) ) ) {
-            return rest_ensure_response( new \WP_Error(
-                'missing_api_key',
-                'A MailChimp API Key is required to complete this request.',
-                [ 'status' => \WP_Http::OK ]
-            ) );
+        if (empty($api_key = Options::getOption(Mailchimp::SETTING_API_KEY))) {
+            return rest_ensure_response(
+                new WP_Error(
+                    'missing_api_key',
+                    'A MailChimp API Key is required to complete this request.',
+                    ['status' => WP_Http::OK]
+                )
+            );
         }
 
         // Count submissions
-        if ( ! $this->canSubmitForm( time() ) ) {
-            return rest_ensure_response( new \WP_Error(
-                'submission_error',
-                'Form submission exceeded. Please try again in an hour.',
-                [ 'status' => \WP_Http::OK ]
-            ) );
+        if (!$this->canSubmitForm(time())) {
+            return rest_ensure_response(
+                new WP_Error(
+                    'submission_error',
+                    'Form submission exceeded. Please try again in an hour.',
+                    ['status' => WP_Http::OK]
+                )
+            );
         }
 
         try {
-            $chimp = new MailChimp( $api_key );
-            $list_id = $this->api->decrypt( $request->get_param( Mailchimp::LIST_ID ) );
-            $email_address = sanitize_email( $request->get_param( self::ROUTE_REQUIRED_FIELD ) );
-            $subscriber = $chimp->subscriberHash( $email_address );
-            $response = $chimp->get( "lists/$list_id/members/$subscriber" );
+            $chimp = new MailChimp($api_key);
+            $list_id = $this->api->decrypt($request->get_param(Mailchimp::LIST_ID));
+            $email_address = sanitize_email($request->get_param(self::ROUTE_REQUIRED_FIELD));
+            $subscriber = $chimp->subscriberHash($email_address);
+            $response = $chimp->get("lists/$list_id/members/$subscriber");
 
             // User is subscribed, send them the download!
-            if ( $chimp->success() && isset( $response['id'] ) ) {
-                $file_url = $this->api->getDecryptFileIdAttachmentUrl( $request );
-                if ( $file_url !== '' ) {
+            if ($chimp->success() && isset($response['id'])) {
+                $file_url = $this->api->getDecryptFileIdAttachmentUrl($request);
+                if ($file_url !== '') {
                     $data['success'] = true;
                     $data['url'] = $this->api->buildDownloadRestUrl(
                         $email_address,
@@ -126,40 +130,39 @@ class SubscriptionController extends RegisterPostRoute {
                         $file_url
                     );
                 }
-                delete_transient( $this->api->getTransientKey() );
+                delete_transient($this->api->getTransientKey());
             } else {
                 // @todo Make this an option from the settings page.
                 $data['message'] = 'It seems you\'re not subscribed.';
             }
-        } catch ( \Exception $e ) {
-            $data['error'] = esc_html( $e->getMessage() );
+        } catch (Exception $e) {
+            $data['error'] = esc_html($e->getMessage());
         }
 
-        return rest_ensure_response( $data );
+        return rest_ensure_response($data);
     }
 
     /**
      * Can the user submit the form request?
-     *
      * @param int $time
-     *
      * @return bool
      */
-    private function canSubmitForm( int $time ): bool {
+    private function canSubmitForm(int $time): bool
+    {
         $key = $this->api->getTransientKey();
-        $transient = get_transient( $key );
-        if ( $transient === false ) {
+        $transient = get_transient($key);
+        if ($transient === false) {
             $transient = [
                 'last_submitted' => 0,
                 'submission_count' => 0,
             ];
         }
 
-        if ( $transient['submission_count'] > Api::MAX_SUBMISSIONS ||
-             (
-                 $time - $transient['last_submitted'] < HOUR_IN_SECONDS &&
-                 $transient['submission_count'] > Api::MAX_SUBMISSIONS
-             )
+        if ($transient['submission_count'] > Api::MAX_SUBMISSIONS ||
+            (
+                $time - $transient['last_submitted'] < HOUR_IN_SECONDS &&
+                $transient['submission_count'] > Api::MAX_SUBMISSIONS
+            )
         ) {
             return false;
         }
@@ -167,7 +170,7 @@ class SubscriptionController extends RegisterPostRoute {
         $transient['last_submitted'] = $time;
         $transient['submission_count'] = $transient['submission_count'] + 1;
 
-        set_transient( $key, $transient, DAY_IN_SECONDS );
+        set_transient($key, $transient, DAY_IN_SECONDS);
 
         return true;
     }
