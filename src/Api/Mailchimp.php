dab8bc2409029c17a39a9328f43ca3131e162dc6
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-21 16:29:59 -0800

    Code cleanup

--- src/Api/Mailchimp.php
+++ src/Api/Mailchimp.php
@@ -1,41 +1,45 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Dwnload\WpEmailDownload\Api;
+
+use Exception;
+use ReflectionClass;
 
 /**
  * Class Mailchimp
- *
  * @package Dwnload\WpEmailDownload\Api
  */
-class Mailchimp extends \DrewM\MailChimp\MailChimp {
+class Mailchimp extends \DrewM\MailChimp\MailChimp
+{
 
-    const LIST_ID = 'list_id';
-    const SETTING_API_KEY = 'api_key';
-    const SETTING_LIST_ID = self::LIST_ID;
+    const string LIST_ID = 'list_id';
+    const string SETTING_API_KEY = 'api_key';
+    const string SETTING_LIST_ID = self::LIST_ID;
 
     /**
      * Create a new instance
-     *
      * @param string $api_key Your MailChimp API key
-     *
-     * @throws \Exception
+     * @throws Exception
      */
-    public function __construct( $api_key ) {
-        parent::__construct( $api_key );
+    public function __construct($api_key)
+    {
+        parent::__construct($api_key);
     }
 
     /**
-     *
      * @return string
      */
-    public function getListsHtml(): string {
-        $html = esc_html__( 'Error connecting to MailChimp.', 'email-download' );
+    public function getListsHtml(): string
+    {
+        $html = esc_html__('Error connecting to MailChimp.', 'email-download');
         $response = $this->getLists();
 
-        if ( isset( $response['lists'] ) ) {
+        if (isset($response['lists'])) {
             $html = '<ul style="margin: 0">';
-            foreach ( $response['lists'] as $list ) {
-                $array[ $list['id'] ] = $list['name'];
+            foreach ($response['lists'] as $list) {
+                $array[$list['id']] = $list['name'];
                 $html .= "<li>{$list['name']} ({$list['id']})</li>";
             }
             $html .= '</ul>';
@@ -48,13 +52,14 @@ class Mailchimp extends \DrewM\MailChimp\MailChimp {
      * @param bool $force Force refresh.
      * @return array
      */
-    public function getListsArray( bool $force = false ): array {
+    public function getListsArray(bool $force = false): array
+    {
         $array = [];
-        $response = $this->getLists( $force );
+        $response = $this->getLists($force);
 
-        if ( isset( $response['lists'] ) ) {
-            foreach ( $response['lists'] as $list ) {
-                $array[ $list['id'] ] = $list['name'];
+        if (isset($response['lists'])) {
+            foreach ($response['lists'] as $list) {
+                $array[$list['id']] = $list['name'];
             }
         }
 
@@ -65,24 +70,23 @@ class Mailchimp extends \DrewM\MailChimp\MailChimp {
      * @param bool $force Force refresh.
      * @return array
      */
-    protected function getLists( bool $force = false ): array {
+    protected function getLists(bool $force = false): array
+    {
         try {
-            $reflection = new \ReflectionClass( parent::class );
-        } catch ( \Exception $exception ) {
+            $reflection = new ReflectionClass(parent::class);
+        } catch (Exception $exception) {
             return [];
         }
-        $api_key = $reflection->getProperty( 'api_key' );
-        $api_key->setAccessible( true );
-        $transient = sprintf( 'dwnload/mailchimp_lists_%s', base64_encode( $api_key ) );
-        $api_key->setAccessible( false );
+        $api_key = $reflection->getProperty('api_key');
+        $transient = sprintf('dwnload/mailchimp_lists_%s', base64_encode($api_key->getValue($this)));
 
-        if ( ( $response = get_transient( $transient ) ) === false ) {
+        if (($response = get_transient($transient)) === false) {
             try {
-                $response = $this->get( 'lists' );
-                if ( is_array( $response ) && $force === false ) {
-                    set_transient( $transient, $response, DAY_IN_SECONDS );
+                $response = $this->get('lists');
+                if (is_array($response) && $force === false) {
+                    set_transient($transient, $response, DAY_IN_SECONDS);
                 }
-            } catch ( \Exception $exception ) {
+            } catch (Exception $exception) {
                 return [];
             }
         }
--- src/Api/Scripts.php
+++ src/Api/Scripts.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Dwnload\WpEmailDownload\Api;
 
 use Dwnload\WpEmailDownload\EmailDownload;
@@ -8,38 +10,40 @@ use TheFrosty\WpUtilities\Plugin\WpHooksInterface;
 
 /**
  * Class Scripts
- *
  * @package Dwnload\WpEmailDownload\Api
  */
-class Scripts implements WpHooksInterface {
+class Scripts implements WpHooksInterface
+{
 
-    const SCRIPT_HANDLE = 'email-download';
-    const OBJECT_NAME = 'emailDownload';
+    const string SCRIPT_HANDLE = 'email-download';
+    const string OBJECT_NAME = 'emailDownload';
 
     /**
      * Add class hooks
      */
-    public function addHooks(): void {
-        add_action( 'wp_enqueue_scripts', [ $this, 'registerScripts' ] );
+    public function addHooks(): void
+    {
+        add_action('wp_enqueue_scripts', [$this, 'registerScripts']);
     }
 
     /**
      * Register Api scripts.
      */
-    public function registerScripts() {
+    public function registerScripts(): void
+    {
         wp_register_script(
             self::SCRIPT_HANDLE,
-            plugins_url( 'assets/js/email-download.js', EmailDownload::getFile() ),
-            [ 'jquery' ]
+            plugins_url('assets/js/email-download.js', EmailDownload::getFile()),
+            ['jquery']
         );
 
-        wp_localize_script( self::SCRIPT_HANDLE, self::OBJECT_NAME, [
-                'root' => esc_url_raw( rest_url() ),
+        wp_localize_script(self::SCRIPT_HANDLE, self::OBJECT_NAME, [
+                'root' => esc_url_raw(rest_url()),
                 'namespace' => EmailDownload::ROUTE_NAMESPACE,
                 'route' => SubscriptionController::ROUTE_PREFIX,
-                'nonce' => wp_create_nonce( RouteService::NONCE_ACTION ),
-                'success' => __( 'Thanks for your submission!', 'your-text-domain' ),
-                'failure' => __( 'Your submission could not be processed.', 'your-text-domain' ),
+                'nonce' => wp_create_nonce(RouteService::NONCE_ACTION),
+                'success' => __('Thanks for your submission!', 'your-text-domain'),
+                'failure' => __('Your submission could not be processed.', 'your-text-domain'),
                 'current_user_id' => get_current_user_id(),
             ]
         );
