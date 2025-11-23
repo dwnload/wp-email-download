59d03f9517111f8e02bbdb4d60ca1d73127273e3
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-22 18:47:24 -0800

    Localize our script data for the block too 👌🏻

--- src/Api/Scripts.php
+++ src/Api/Scripts.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Dwnload\WpEmailDownload\Api;
 
+use Dwnload\WpEmailDownload\Blocks\EmailDownload as EmailDownloadBlock;
 use Dwnload\WpEmailDownload\EmailDownload;
 use Dwnload\WpEmailDownload\RestApi\SubscriptionController;
 use TheFrosty\WpUtilities\Plugin\WpHooksInterface;
+use WP_Block_Type_Registry;
+use function __;
+use function esc_url_raw;
 use function filemtime;
+use function get_current_user_id;
 use function plugin_dir_path;
 use function plugins_url;
+use function rest_url;
+use function wp_create_nonce;
 use function wp_register_style;
 
 /**
@@ -47,15 +54,29 @@ class Scripts implements WpHooksInterface
             ver: filemtime(plugin_dir_path(EmailDownload::getFile()) . 'assets/js/email-download.js')
         );
 
-        wp_localize_script(self::SCRIPT_HANDLE, self::OBJECT_NAME, [
-                'root' => esc_url_raw(rest_url()),
-                'namespace' => EmailDownload::ROUTE_NAMESPACE,
-                'route' => SubscriptionController::ROUTE_PREFIX,
-                'nonce' => wp_create_nonce('wp_rest'),
-                'success' => __('Thanks for your submission!', 'email-download'),
-                'failure' => __('Your submission could not be processed.', 'email-download'),
-                'current_user_id' => get_current_user_id(),
-            ]
-        );
+        // Localize our script data to each registered script (our shortcode & block).
+        $handles = [self::SCRIPT_HANDLE];
+        $registry = WP_Block_Type_Registry::get_instance();
+        if (
+            $registry &&
+            $block = $registry->get_all_registered()[EmailDownloadBlock::NAME] ?? null
+        ) {
+            $handles = array_merge($handles, $block->view_script_handles);
+        }
+        foreach ($handles as $handle) {
+            wp_localize_script(
+                $handle,
+                self::OBJECT_NAME,
+                [
+                    'root' => esc_url_raw(rest_url()),
+                    'namespace' => EmailDownload::ROUTE_NAMESPACE,
+                    'route' => SubscriptionController::ROUTE_PREFIX,
+                    'nonce' => wp_create_nonce('wp_rest'),
+                    'success' => __('Thanks for your submission!', 'email-download'),
+                    'failure' => __('Your submission could not be processed.', 'email-download'),
+                    'current_user_id' => get_current_user_id(),
+                ]
+            );
+        }
     }
 }
